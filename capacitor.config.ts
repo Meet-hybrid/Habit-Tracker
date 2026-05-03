93d90db62fe68b8cc485ffd463e4be4e6f3cf481
michael philip <philipmichaelonyekachi>
2026-05-04 00:09:21 +0100

    Initial commit for Appflow build

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.meethybrid.habittracker',
+  appName: 'Habit Tracker',
+  webDir: '.next'
+};
+
+export default config;
